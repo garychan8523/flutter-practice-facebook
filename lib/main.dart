53c09e9aa6daf0aed53fd60574462fc8a64f932f
gary chan <garychan8523@gmail.com>
2020-10-02 03:18:16 +0800

    add desktop responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
-
-import 'screen/home_screen.dart';
+import 'package:flutter_facebook_responsive_ui/screen/screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Palette.scaffold,
       ),
-      home: HomeScreen(),
+      home: NavScreen(),
     );
   }
 }
--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 
 class Stories extends StatelessWidget {
   final User currentUser;
@@ -18,20 +19,15 @@ class Stories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200.0,
-      color: Colors.white,
+      color: Responsive.isDesktop(context) ? Colors.transparent : Colors.white,
       child: ListView.builder(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10.0,
-          horizontal: 8.0,
-        ),
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
         scrollDirection: Axis.horizontal,
         itemCount: 1 + stories.length,
         itemBuilder: (BuildContext context, int index) {
           if (index == 0) {
             return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 4.0,
-              ),
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
               child: _StoryCard(
                 isAddStory: true,
                 currentUser: currentUser,
@@ -84,6 +80,15 @@ class _StoryCard extends StatelessWidget {
           decoration: BoxDecoration(
             gradient: Palette.storyGradient,
             borderRadius: BorderRadius.circular(12.0),
+            boxShadow: Responsive.isDesktop(context)
+                ? const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 4.0,
+                    ),
+                  ]
+                : null,
           ),
         ),
         Positioned(
